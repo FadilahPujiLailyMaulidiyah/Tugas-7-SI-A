b4e2cb5f11916602eaf1ff3a1ddabd7c28bbeac3
Fadilah Puji Laily Maulidiyah <fadilahpjl@gmail.com>
2025-04-27 10:44:21 +0700

    home.php

---
+++ views/home.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="assets/style.css">
+<div class="container">
+    <h3>Home - Peminjaman Barang</h3>
+    <a href="?c=Peminjaman" class="btn">Lihat Daftar Peminjaman</a>
+</div>
